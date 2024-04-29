8a7b83188a1d1335de6584f249a4b443bd1ff65f
dimasyoga394@gmail.com <dimasyoga394@gmail.com>
2024-04-29 09:02:26 +0700

    add

--- surah/6.js
+++ surah/6.js
@@ -5,15 +5,15 @@ function fetchSurahAndDisplay(surahNumber) {
     fetch(`https://quran-api.santrikoding.com/api/surah/${surahNumber}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+           // console.log(data);
             const surah = data.ayat;
             const audio = data.audio;
-            console.log(audio);
-            au.innerHTML += `<audio controls>
-                <source src="${audio}" type="audio/mpeg">
-                Browsermu tidak mendukung tag audio, upgrade donk!
-            </audio>`;
-            console.log(surah);
+           // console.log(audio);
+          //  au.innerHTML += `<audio controls>
+              //  <source src="${audio}" type="audio/mpeg">
+             //   Browsermu tidak mendukung tag audio, upgrade donk!
+          //  </audio>`;
+           // console.log(surah);
             for (let i = 0; i < surah.length; i++) {
                 const ayatDiv = document.createElement('div');
                 const ayat = surah[i].ar;
